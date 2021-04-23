af0675f2f2bd7d7e94a3408be5cd9552ad06827e
rodionova <rodionova@enterego.ru>
2021-04-23 12:52:04 +0300

    fix import -delete planned date

--- wp-content/plugins/woo-sync-1c/exchange/import.php
+++ wp-content/plugins/woo-sync-1c/exchange/import.php
@@ -996,7 +996,7 @@ function wc1c_replace_product($is_full, $guid, $product, $wc1c_ar_options)
         foreach ($product['ЗначенияСвойств'] as $property) {
 
             if($property['Ид'] == "first_date" && is_array($property['Значение']) && !empty($property['Значение'][0])) {
-                delete_post_meta($post_id, 'planned_date','');
+                delete_post_meta($post_id, 'planned_date');
                 update_post_meta($post_id, 'first_date',date("d.m.Y", strtotime($property['Значение'][0])));
                 continue;
             }
