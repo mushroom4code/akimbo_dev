9139d677e7b265fa01e3b8a07f761ff10bb99888
katya <rodionova@enterego.ru>
2021-10-22 12:21:58 +0300

    поправлено условие  на вывод надписи с учетом статуса товара

--- wp-content/themes/salient/woocommerce/loop/price.php
+++ wp-content/themes/salient/woocommerce/loop/price.php
@@ -29,9 +29,10 @@ $emptyStock = array();
 $sum = intval($product->get_stock_quantity() - $product->get_backorders_quantity());
 foreach ($variations1 as $value) {
     $single_variation = new WC_Product_Variation($value);
-    if ($sum <= 0) {
+    if ($sum <= 0 && $single_variation->stock_status == 'onbackorder') {
         $infoMessage = 'Скоро в наличии';
-    } elseif ($single_variation->stock_status == 'outofstock') {
+    }
+    if ($single_variation->stock_status == 'outofstock') {
         $emptyStock [] = 1;
     }
     $i++;
