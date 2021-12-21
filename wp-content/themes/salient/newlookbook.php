d16a0c8ec8c35acf8ad24e0e46a6c4c6d1bace5c
katya <rodionova@enterego.ru>
2021-12-21 10:20:41 +0300

    временно возвращен старый вариант  вывода лукбуков (на сервере пока есть файлы - необходимо сделать git pull)

--- wp-content/themes/salient/newlookbook.php
+++ wp-content/themes/salient/newlookbook.php
@@ -1,68 +1,68 @@
-<?php ///* Template Name: EeLookBook */
-//// Exit if accessed directly.
-//if(!defined('ABSPATH')) {
-//    exit;
-//}
-//
-//get_header();
-//nectar_page_header($post->ID);
-//
-//$nectar_fp_options = nectar_get_full_page_options(); ?>
-<!---->
-<!--<div class="container-wrap">-->
-<!--    <div class="row">-->
-<!--        <div style="display: flex;justify-content: center;align-items: center">-->
-<!---->
-<!--            <div class="forCollection">-->
-<!--                <a href="/look-collection-35/" class="one-look-father">-->
-<!--                    <div class="container-for-background"-->
-<!--                         style='background: url("/wp-content/uploads/2020/09/1.png") no-repeat;background-size: cover; background-position: center;'>-->
-<!--                        <span class="name-look-father">Красный бархат</span>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--                <a href="/look-collection-36/" class="one-look-father">-->
-<!--                    <div class="container-for-background"-->
-<!--                         style='background: url("/wp-content/uploads/2020/09/2.png") no-repeat;background-size: cover; background-position: center;'>-->
-<!--                        <span class="name-look-father">Халва</span>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--                <a href="/look-collection-37/" class="one-look-father">-->
-<!--                    <div class="container-for-background"-->
-<!--                         style='background: url("/wp-content/uploads/2020/09/3.png") no-repeat;background-size: cover; background-position: center;'>-->
-<!--                        <span class="name-look-father">Черничный чизкейк</span>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--                <a href="/look-collection-38/" class="one-look-father">-->
-<!--                    <div class="container-for-background"-->
-<!--                         style='background: url("/wp-content/uploads/2020/09/4.png") no-repeat;background-size: cover; background-position: center;'>-->
-<!--                        <span class="name-look-father">Тыквенный пирог</span>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--                <a href="/look-collection-39/" class="one-look-father">-->
-<!--                    <div class="container-for-background"-->
-<!--                         style='background: url("/wp-content/uploads/2020/09/5.png") no-repeat;background-size: cover; background-position: center;'>-->
-<!--                        <span class="name-look-father">Ягодная панакота</span>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--                <a href="/look-collection-40/" class="one-look-father">-->
-<!--                    <div class="container-for-background"-->
-<!--                         style='background: url("/wp-content/uploads/2020/09/6.png") no-repeat;background-size: cover; background-position: center;'>-->
-<!--                        <span class="name-look-father">Тирамису</span>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--                <a href="/look-collection-41/" class="one-look-father">-->
-<!--                    <div class="container-for-background"-->
-<!--                         style='background: url("/wp-content/uploads/2020/09/7.png") no-repeat;background-size: cover; background-position: center;'>-->
-<!--                        <span class="name-look-father">Малиновый капкейк</span>-->
-<!--                    </div>-->
-<!--                </a>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
-<!---->
-<!---->
-<?php //get_footer(); ?>
-<!---->
-<!---->
-<!---->
+<?php /* Template Name: EeLookBook */
+// Exit if accessed directly.
+if(!defined('ABSPATH')) {
+    exit;
+}
+
+get_header();
+nectar_page_header($post->ID);
+
+$nectar_fp_options = nectar_get_full_page_options(); ?>
+
+<div class="container-wrap">
+    <div class="row">
+        <div style="display: flex;justify-content: center;align-items: center">
+
+            <div class="forCollection">
+                <a href="/look-collection-35/" class="one-look-father">
+                    <div class="container-for-background"
+                         style='background: url("/wp-content/uploads/2020/09/1.png") no-repeat;background-size: cover; background-position: center;'>
+                        <span class="name-look-father">Красный бархат</span>
+                    </div>
+                </a>
+                <a href="/look-collection-36/" class="one-look-father">
+                    <div class="container-for-background"
+                         style='background: url("/wp-content/uploads/2020/09/2.png") no-repeat;background-size: cover; background-position: center;'>
+                        <span class="name-look-father">Халва</span>
+                    </div>
+                </a>
+                <a href="/look-collection-37/" class="one-look-father">
+                    <div class="container-for-background"
+                         style='background: url("/wp-content/uploads/2020/09/3.png") no-repeat;background-size: cover; background-position: center;'>
+                        <span class="name-look-father">Черничный чизкейк</span>
+                    </div>
+                </a>
+                <a href="/look-collection-38/" class="one-look-father">
+                    <div class="container-for-background"
+                         style='background: url("/wp-content/uploads/2020/09/4.png") no-repeat;background-size: cover; background-position: center;'>
+                        <span class="name-look-father">Тыквенный пирог</span>
+                    </div>
+                </a>
+                <a href="/look-collection-39/" class="one-look-father">
+                    <div class="container-for-background"
+                         style='background: url("/wp-content/uploads/2020/09/5.png") no-repeat;background-size: cover; background-position: center;'>
+                        <span class="name-look-father">Ягодная панакота</span>
+                    </div>
+                </a>
+                <a href="/look-collection-40/" class="one-look-father">
+                    <div class="container-for-background"
+                         style='background: url("/wp-content/uploads/2020/09/6.png") no-repeat;background-size: cover; background-position: center;'>
+                        <span class="name-look-father">Тирамису</span>
+                    </div>
+                </a>
+                <a href="/look-collection-41/" class="one-look-father">
+                    <div class="container-for-background"
+                         style='background: url("/wp-content/uploads/2020/09/7.png") no-repeat;background-size: cover; background-position: center;'>
+                        <span class="name-look-father">Малиновый капкейк</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
+
+
+
