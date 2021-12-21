bd1e77ffa8591277f7a9d367ae6f7c7e7d3e3cb6
katya <rodionova@enterego.ru>
2021-12-21 20:23:57 +0300

    новые капсулы 10

---
+++ wp-content/themes/salient/custom-lookbooks-pages/look-collections-10.php
@@ -0,0 +1,122 @@
+<?php /* Template Name: look-collections-10  */
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+get_header();
+nectar_page_header($post->ID);
+$nectar_fp_options = nectar_get_full_page_options();
+
+?>
+<div class="container-wrap">
+    <a href='/lookbook' class=back-url>К списку коллекций</a>
+    <div class="row" style="padding-bottom: 200px">
+        <div style='display: flex; margin-left: 11%; flex-direction: column; width: 80%;'>
+            <h1>Фраппе</h1>
+            <p class="intro-text">
+                Здесь есть сразу два костюма, которые станут самодостаточным и готовым решением. В одном из них
+                представлены брюки с утяжкой на щиколотках, а во втором с карманами в стиле карго. Футболка с цветочным
+                принтом и трикотажными манжетами мимикрирует под спортивный и женственный стиль в зависимости от обуви.
+            </p>
+        </div>
+        <div style="display: flex;justify-content: center;align-items: center">
+            <div class="forCollection">
+                <div class="bodyForOneEl"
+                     style=' background: url("/wp-content/uploads/2021/11/09_12_2021_AKIMBO_44797.jpg") no-repeat;
+                     background-size: cover;background-position: center;'>
+                    <a href="/shop/bluzka-d-2054-pre-vl/" target="_blank" data-tooltip="Блузка Д-2054 ПРЕ(ВЛ)"
+                       style="height: 100%; bottom: 20px;"
+                       data-position="right" class="right custPoint">
+                        <i class="fa fa-bullseye" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="bodyForOneEl"
+                     style=' background: url("/wp-content/uploads/2021/11/09_12_2021_AKIMBO_44467.jpg") no-repeat;
+                     background-size: cover;background-position: center;'>
+                    <a href="/shop/bluzka-d-2057-ov-vl/" target="_blank" data-tooltip="Блузка Д-2057 ОВ(ВЛ)"
+                       style="height: 100%;"
+                       data-position="right" class="right custPoint">
+                        <i class="fa fa-bullseye" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="bodyForOneEl"
+                     style=' background: url("/wp-content/uploads/2021/11/09_12_2021_AKIMBO_44460.jpg") no-repeat;
+                     background-size: cover;background-position: center;'>
+                    <a href="/shop/bryuki-b-2013-ov-vl/" target="_blank" data-tooltip="Брюки Б-2013 ОВ(ВЛ)"
+                       style="height: 100%;bottom: -60px;left: 30px;"
+                       data-position="right" class="right custPoint">
+                        <i class="fa fa-bullseye" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="bodyForOneEl"
+                     style=' background: url("/wp-content/uploads/2021/11/09_12_2021_AKIMBO_44574.jpg") no-repeat;
+                     background-size: cover;background-position: center;'>
+                    <a href="/shop/zhaket-v-922-kam-vl/" target="_blank" data-tooltip="Жакет В-922 КАМ(ВЛ)"
+                       style="height: 100%;bottom: 60px;left: 30px;"
+                       data-position="right" class="right custPoint">
+                        <i class="fa fa-bullseye" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="bodyForOneEl"
+                     style=' background: url("/wp-content/uploads/2021/11/09_12_2021_AKIMBO_44554.jpg") no-repeat;
+                     background-size: cover;background-position: center;'>
+                    <a href="/shop/bryuki-b-2015-kam-vl/" target="_blank" data-tooltip="Брюки Б-2015 КАМ(ВЛ)"
+                       style="height: 100%;bottom: -60px;left: -25px;"
+                       data-position="right" class="right custPoint">
+                        <i class="fa fa-bullseye" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="bodyForOneEl"
+                     style=' background: url("/wp-content/uploads/2021/11/09_12_2021_AKIMBO_44513.jpg") no-repeat;
+                     background-size: cover;background-position: center;'>
+                    <a href="/shop/bluzka-d-2053-lil-vl/" target="_blank" data-tooltip="Блузка Д-2053 ЛИЛ(ВЛ)"
+                       style="height: 100%;left: -25px;"
+                       data-position="right" class="right custPoint">
+                        <i class="fa fa-bullseye" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="bodyForOneEl"
+                     style=' background: url("/wp-content/uploads/2021/11/09_12_2021_AKIMBO_44510.jpg") no-repeat;
+                     background-size: cover;background-position: center;'>
+                    <a href="/shop/bryuki-b-2012-lil-vl/" target="_blank" data-tooltip="Брюки Б-2012 ЛИЛ(ВЛ)"
+                       style="height: 100%;bottom: -60px;left: 35px;"
+                       data-position="right" class="right custPoint">
+                        <i class="fa fa-bullseye" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="bodyForOneEl"
+                     style=' background: url("/wp-content/uploads/2021/11/09_12_2021_AKIMBO_44848.jpg") no-repeat;
+                     background-size: cover;background-position: center;'>
+                    <a href="/shop/zhaket-v-922-ver-vl/" target="_blank" data-tooltip="Жакет В-922 ВЕР(ВЛ)"
+                       style="height: 100%;bottom: 60px;left: 35px;"
+                       data-position="right" class="right custPoint">
+                        <i class="fa fa-bullseye" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="bodyForOneEl"
+                     style=' background: url("/wp-content/uploads/2021/11/09_12_2021_AKIMBO_44828.jpg") no-repeat;
+                     background-size: cover;background-position: center;'>
+                    <a href="/shop/bryuki-b-2015-ver-vl/" target="_blank" data-tooltip="Брюки Б-2015 ВЕР(ВЛ)"
+                       style="height: 100%;bottom: -60px;left: 60px;"
+                       data-position="right" class="right custPoint">
+                        <i class="fa fa-bullseye" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="bodyForOneEl"
+                     style=' background: url("/wp-content/uploads/2021/11/09_12_2021_AKIMBO_45727.jpg") no-repeat;
+                     background-size: cover;background-position: center;'>
+                    <a href="/shop/plate-p-2057-al-vl/" target="_blank" data-tooltip="Платье П-2057 АЛ(ВЛ)"
+                       style="height: 100%;bottom: 70px;left: 30px;"
+                       data-position="right" class="right custPoint">
+                        <i class="fa fa-bullseye" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </div><!--/row-->
+</div><!--/container-->
+
+<?php get_footer(); ?>
+
