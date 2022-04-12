678d0c8f9e582d012067480453c7e1f96d987ae0
katya <rodionova@enterego.ru>
2022-04-12 11:38:06 +0300

    #000016382 -убрана цна если пользовтаель неавторизован

--- wp-content/themes/salient/nectar/nectar-vc-addons/vc_templates/nectar_food_menu_item.php
+++ wp-content/themes/salient/nectar/nectar-vc-addons/vc_templates/nectar_food_menu_item.php
@@ -1,8 +1,15 @@
 <?php
 extract(shortcode_atts(array("style" => "default", "item_name" => '', 'item_name_heading_tag' => 'h4', "item_price" => "", 'item_description' => '', 'class' => ''), $atts));
-
-
+$price = '';
+if(is_user_logged_in()){
+    $price = '<div class="item_price"><' . $item_name_heading_tag . '>' . $item_price . '</' . $item_name_heading_tag . '></div>';
+}
 $line_markup = '<div class="line_spacer"></div>';
 
-echo '<div class="nectar_food_menu_item '.$class.'" data-style="'.$style.'"><div class="inner"><div class="item_name"><'.$item_name_heading_tag.'>'.$item_name.'</'.$item_name_heading_tag.'></div>'.$line_markup.'<div class="item_price"><'.$item_name_heading_tag.'>'.$item_price.'</'.$item_name_heading_tag.'></div></div><div class="item_description">'.$item_description.'</div></div>';
+echo '<div class="nectar_food_menu_item ' . $class . '" data-style="' . $style . '">
+<div class="inner"><div class="item_name">
+<' . $item_name_heading_tag . '>' . $item_name . '</' . $item_name_heading_tag . '>
+</div>' . $line_markup . $price.'</div>
+<div class="item_description">' . $item_description . '</div>
+</div>';
 ?>
