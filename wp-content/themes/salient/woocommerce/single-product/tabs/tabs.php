af14a2d991a619d55c13cd9c337f78fb64fce1e4
karpenko a <ak@enterego.ru>
2022-05-04 13:49:33 +0000

    mini edet3

--- wp-content/themes/salient/woocommerce/single-product/tabs/tabs.php
+++ wp-content/themes/salient/woocommerce/single-product/tabs/tabs.php
@@ -22,34 +22,30 @@ $tabs = apply_filters( 'woocommerce_product_tabs', array() );
 global $nectar_options;
 
 $tab_style = (!empty($nectar_options['product_tab_position'])) ? $nectar_options['product_tab_position'] : 'default';
-$fullwidth_tabs = (!empty($nectar_options['product_tab_position']) && $nectar_options['product_tab_position'] == 'fullwidth' || !empty($nectar_options['product_tab_position']) && $nectar_options['product_tab_position'] == 'fullwidth_centered') ? true : false;
+$fullwidth_tabs = (!empty($nectar_options['product_tab_position']) && $nectar_options['product_tab_position'] == 'fullwidth'
+ || !empty($nectar_options['product_tab_position']) && $nectar_options['product_tab_position'] == 'fullwidth_centered') ? true : false;
 
-if ( ! empty( $tabs ) ) : ?>
-
-	<div class="woocommerce-tabs wc-tabs-wrapper <?php if($fullwidth_tabs == true) echo 'full-width-tabs'; ?>" data-tab-style="<?php echo esc_attr( $tab_style ); ?>">
-
-		<?php if($fullwidth_tabs == true) echo '<div class="full-width-content" data-tab-style="'. $tab_style .'"> <div class="tab-container container">'; ?>
-
+if (!empty($tabs)) { ?>
+</div>
+	<div class="woocommerce-tabs wc-tabs-wrapper <?php if($fullwidth_tabs == true) {echo 'full-width-tabs';}?>" data-tab-style="<?= esc_attr( $tab_style ); ?>">
+		<?php if($fullwidth_tabs == true) {
+            echo '<div class="full-width-content" data-tab-style="'. $tab_style .'"> 
+                    <div class="tab-container container">';}?>
 			<ul class="tabs">
-				<?php foreach ( $tabs as $key => $tab ) : ?>
-
-					<li class="<?php echo esc_attr( $key ); ?>_tab">
-						<a href="#tab-<?php echo esc_attr( $key ); ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', $tab['title'], $key ); ?></a>
+				<?php foreach ( $tabs as $key => $tab ) {?>
+					<li class="<?= esc_attr( $key ); ?>_tab">
+						<a href="#tab-<?= esc_attr( $key ); ?>">
+                            <?= apply_filters( 'woocommerce_product_' . $key . '_tab_title', $tab['title'], $key ); ?>
+                        </a>
 					</li>
-
-				<?php endforeach; ?>
+				<?php } ?>
 			</ul>
-		
-		<?php if($fullwidth_tabs == true) echo '</div></div>'; ?>
-
-		<?php foreach ( $tabs as $key => $tab ) : ?>
-
-			<div class="panel entry-content" id="tab-<?php echo esc_attr( $key ); ?>">
+		<?php if($fullwidth_tabs == true) {
+            echo '</div></div>';
+        } foreach ( $tabs as $key => $tab ) { ?>
+			<div class="panel entry-content" id="tab-<?= esc_attr( $key ); ?>">
 				<?php call_user_func( $tab['callback'], $key, $tab ); ?>
 			</div>
-
-		<?php endforeach; ?>
-		
+		<?php } ?>
 	</div>
-
-<?php endif; ?>
+<?php }
