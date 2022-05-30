0d9909fddfb8fb2028d368fc320435cb29e0e634
katya <rodionova@enterego.ru>
2022-05-30 14:43:56 +0300

    #000016628 - убран вывод в письме размеров

--- wp-content/themes/salient/woocommerce/emails/email-order-details.php
+++ wp-content/themes/salient/woocommerce/emails/email-order-details.php
@@ -11,13 +11,11 @@
  * the readme will list any important changes.
  *
  * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates/Emails
- * @version 3.3.1
+ * @package WooCommerce\Templates\Emails
+ * @version 3.7.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
 $text_align = is_rtl() ? 'right' : 'left';
 
