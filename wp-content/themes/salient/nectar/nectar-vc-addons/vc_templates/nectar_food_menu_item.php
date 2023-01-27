b016410799fb0ef94c464b3006443375d398c67c
Pavel Vladimirovich <sawel@yandex.ru>
2023-01-27 15:23:31 +0800

    #18903 - цена из id

--- wp-content/themes/salient/nectar/nectar-vc-addons/vc_templates/nectar_food_menu_item.php
+++ wp-content/themes/salient/nectar/nectar-vc-addons/vc_templates/nectar_food_menu_item.php
@@ -2,6 +2,17 @@
 extract(shortcode_atts(array("style" => "default", "item_name" => '', "item_id" => '', 'item_name_heading_tag' => 'h4', "item_price" => "", 'item_description' => '', 'class' => ''), $atts));
 $price = '';
 $item_id = trim($item_id);
+#18903
+if ($item_id !== '') {
+    $prod = wc_get_product($item_id);
+    if ($prod) {
+        $item_price = $prod->get_price();
+        if ($item_price !== '') {
+            $item_price .= ' ₽';
+        }
+    }
+}
+
 $planned_date = get_post_meta($item_id, 'planned_date', true);
 if ($planned_date !== '' && $planned_date !== false) {
     $planned_date = '<div style="text-align: center; padding-top: 5px" class="plain_date">
@@ -15,7 +26,7 @@ if (is_user_logged_in()) {
 }
 $line_markup = '<div class="line_spacer"></div>';
 
-echo '<div class="nectar_food_menu_item ' . $class . '" data-style="' . $style . '">
+echo '<div class="rerik nectar_food_menu_item ' . $class . '" data-style="' . $style . '">
 <div class="inner tets"><div class="item_name">
 <' . $item_name_heading_tag . '>' . $item_name . '</' . $item_name_heading_tag . '>
 </div>' . $line_markup . $price . ' ' . $planned_date . '</div>
