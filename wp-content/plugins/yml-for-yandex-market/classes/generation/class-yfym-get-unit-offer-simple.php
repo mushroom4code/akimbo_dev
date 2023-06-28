c20e580ded7fcdf3d66382df9c69265875743250
erodionova <rodionova@enterego.ru>
2023-06-28 15:16:15 +0300

    #000019862 - групп прайс

--- wp-content/plugins/yml-for-yandex-market/classes/generation/class-yfym-get-unit-offer-simple.php
+++ wp-content/plugins/yml-for-yandex-market/classes/generation/class-yfym-get-unit-offer-simple.php
@@ -242,15 +242,15 @@ class YFYM_Get_Unit_Offer_Simple extends YFYM_Get_Unit_Offer {
             $result_xml .= $this->get_vendor();
             $result_xml .= $this->get_weight();
             $result_xml .= $this->get_dimensions();
+            $result_xml .= '<model>' . $this->get_offer()->get_title() . '</model>' . PHP_EOL;
             $result_xml .= '<variant>' . PHP_EOL;
             $result_xml .= $this->get_amount();
             $result_xml .= '<param name="Длина изделия">' . $length . '</param>'.PHP_EOL;
             $result_xml .= '<param name="Длина по внутреннему шву">' . $length_inner . '</param>'.PHP_EOL;
             $result_xml .= '<param name="Длина по внешнему шву">' . $length_outer . '</param>'.PHP_EOL;
-            $result_xml .= '<barcode>' . $barcode . '</barcode>' . PHP_EOL;
             $result_xml .= '<size name="Размер">' . $size . '</size>' . PHP_EOL;
-            $result_xml .= '<model>' . $this->get_offer()->get_title() . '</model>' . PHP_EOL;
             $result_xml .= '<quantity>' . $this->get_offer()->get_stock_quantity() . '</quantity>' . PHP_EOL;
+            $result_xml .= '<barcode>' . $barcode . '</barcode>' . PHP_EOL;
             $result_xml .= '</variant>' . PHP_EOL;
             $result_xml .= $this->get_vendorcode();
             $result_xml .= $this->get_store();
